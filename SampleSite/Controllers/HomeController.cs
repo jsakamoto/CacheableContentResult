d774a5f3fef506698ea690763b1c6f2a17525000
jsakamoto <junichi.jp@gmail.com>
2017-04-24 22:07:19 +0900

    Update sample site.

--- SampleSite/Controllers/HomeController.cs
+++ SampleSite/Controllers/HomeController.cs
@@ -1,33 +1,13 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Web;
 using System.Web.Mvc;
-using Toolbelt.Web;
 
 namespace SampleSite.Controllers
 {
-    using IO = System.IO;
-
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult CustomImage()
-        {
-            var imagePath = HttpContext.Server.MapPath("~/Content/Adelie.png");
-            var imageBin = IO.File.ReadAllBytes(imagePath);
-            var etag = Convert.ToBase64String(MD5.Create().ComputeHash(imageBin));
-
-            return new CacheableContentResult(
-                    cacheability: HttpCacheability.ServerAndPrivate,
-                    lastModified: IO.File.GetLastWriteTime(imagePath),
-                    etag:etag,
-                    contentType: "image/png",
-                    getContent: () => imageBin
-                );
         }
     }
 }
---
+++ SampleSite/Controllers/UserprofileCOntroller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using Toolbelt.Web;
+
+namespace SampleSite.Controllers
+{
+    public class UserprofileController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            ViewBag.UserId = id;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Picture(int id)
+        {
+            var imagePath = HttpContext.Server.MapPath($"~/App_Data/user/{id}/picture.png");
+            var lastWriteTime = System.IO.File.GetLastWriteTimeUtc(imagePath);
+
+            return new CacheableContentResult(
+                contentType: "image/png",
+                lastModified: lastWriteTime,
+                getContent: () =>
+                {
+                    return System.IO.File.ReadAllBytes(imagePath);
+                }
+            );
+        }
+    }
+}
